7d171adc6e82d6db44e37764ff29825c1e47b4a9
Javier <alexanderjmzc@gmail.com>
2024-08-19 11:55:05 -0500

    Project create

---
+++ src/components/principal/principal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './principal.css'
+
+const Principal = () => {
+  return (
+    <div className='main'>
+        <button>Descargar</button>
+    </div>
+  )
+}
+
+export default Principal
